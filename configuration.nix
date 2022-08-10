88ea1532fb9f99b1d00aa21f880626c7581c7a6e
Shammyshanks <thorinbriggz@gmail.com>
2022-08-10 14:39:38 -0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -13,9 +13,17 @@
     ];
 
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.grub = {
+  enable = true;
+  device = "/dev/sda";
+  useOSProber = true;
+  theme = pkgs.fetchFromGitHub {
+    owner = "shvchk";
+    repo = "fallout-grub-theme";
+    rev = "80734103d0b48d724f0928e8082b6755bd3b2078";
+    sha256 = "sha256-7kvLfD6Nz4cEMrmCA9yq4enyqVyqiTkVZV5y4RyUatU=";
+  };
+ };
   # Bluetooth
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
